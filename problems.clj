8ba7f5505ce034c8b9c0bd6a091c5ec3cc77e126
Ivan Galban <ivan.galban.smith@gmail.com>
2020-04-08 16:23:41 -0400

    Add problem #132

--- problems.clj
+++ problems.clj
@@ -1723,6 +1723,24 @@ your function should return nil"
                                         ; 00000      00000
                   ]}
 
+   {:_id         132
+    :title       "Intervals"
+    :description "Write a function that takes a two-argument predicate,
+a value, and a collection; and returns a new collection where the value
+is inserted between every two items that satisfy the predicate."
+    :tests       ["(= '(1 :less 6 :less 7 4 3) (__ < :less [1 6 7 4 3]))"
+                  "(= '(2) (__ > :more [2]))"
+                  "(= [0 1 :x 2 :x 3 :x 4]  (__ #(and (pos? %) (< % %2)) :x (range 5)))"
+                  "(empty? (__ > :more ()))"
+                  "(= [0 1 :same 1 2 3 :same 5 8 13 :same 21]
+                      (take 12 (->> [0 1]
+                                    (iterate (fn [[a b]] [b (+ a b)]))
+                                    (map first) ; fibonacci numbers
+                                    (__ (fn [a b] ; both even or both odd
+                                          (= (mod a 2) (mod b 2)))
+                                        :same))))"]
+    :tags        ["medium" "seqs core-functions"]}
+
    {:_id         137
     :title       "Digits and bases"
     :description "Write a function which returns a sequence of digits
