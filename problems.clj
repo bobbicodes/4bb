952dc3de5abedcd1939dc4b6d50a8512ee1f4c61
Ivan Galban <ivan.galban.smith@gmail.com>
2020-04-08 17:13:48 -0400

    Add problem #148

--- problems.clj
+++ problems.clj
@@ -1861,6 +1861,26 @@ from the first function after it hits the end."
                   "(= (take 12 (__ 0 inc dec inc dec inc)) [0 1 0 1 0 1 2 1 2 1 2 3])"]
     :tags        ["medium" "sequences"]}
 
+   {:_id         148
+    :title       "The Big Divide"
+    :description "Write a function which calculates the sum of all natural
+numbers under n (first argument) which are evenly divisible by at least one
+of a and b (second and third argument). Numbers a and b are guaranteed to be coprimes.
+
+Note: Some test cases have a very large n, so the most obvious solution will
+exceed the time limit."
+    :tests       ["(= 0 (__ 3 17 11))"
+                  "(= 23 (__ 10 3 5))"
+                  "(= 233168 (__ 1000 3 5))"
+                  "(= \"2333333316666668\" (str (__ 100000000 3 5)))"
+                  "(= \"110389610389889610389610\"
+                      (str (__ (* 10000 10000 10000) 7 11)))"
+                  "(= \"1277732511922987429116\"
+                      (str (__ (* 10000 10000 10000) 757 809)))"
+                  "(= \"4530161696788274281\"
+                      (str (__ (* 10000 10000 1000) 1597 3571)))"]
+    :tags        ["medium" "math"]}
+
    {:_id         158
     :title       "Decurry"
     :description "Write a function that accepts a curried function of unknown arity n.
