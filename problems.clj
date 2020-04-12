d48098cb6f29efd43d78f01a75cd7fbd625d3f6b
Ivan Galban <ivan.galban.smith@gmail.com>
2020-04-12 02:01:32 -0400

    Add problem #135

--- problems.clj
+++ problems.clj
@@ -1781,6 +1781,20 @@ the map contains an entry with that key and its value is nil."
                   "(false? (__ :c {:a nil :b 2}))"]
     :tags        ["elementary" "maps"]}
 
+   {:_id         135
+    :title       "Infix Calculator"
+    :description "Your friend Joe is always whining about Lisps using the prefix
+notation for math. Show him how you could easily write a function that does math
+using the infix notation. Is your favorite language that flexible, Joe? Write a
+function that accepts a variable length mathematical expression consisting of
+numbers and the operations +, -, *, and /. Assume a simple calculator that does
+not do precedence and instead just calculates left to right."
+    :tests       ["(= 7  (__ 2 + 5))"
+                  "(= 42 (__ 38 + 48 - 2 / 2))"
+                  "(= 8  (__ 10 / 2 - 1 * 2))"
+                  "(= 72 (__ 20 / 2 + 2 + 4 + 8 - 6 - 10 * 9))"]
+    :tags        ["easy" "higher-order-functions math"]}
+
    {:_id         137
     :title       "Digits and bases"
     :description "Write a function which returns a sequence of digits
